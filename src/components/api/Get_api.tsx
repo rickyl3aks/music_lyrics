39d5f6106872e491d222e33ca6a18b7063d4bdec
Riccardo Consolandi <rc@capsula.app>
2022-10-15 11:49:59 +0200

    fix: getApi, trackApi; add extra link to deal with cors error

--- src/components/api/Get_api.tsx
+++ src/components/api/Get_api.tsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 
 export const Get_api = () => {
-  const [lyrics, setLyrics] = useState({});
+  const [lyrics, setLyrics] = useState<any>();
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState<unknown>();
 
   const get_lyrics = async () => {
     try {
       const res = await fetch(
-        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MUSIXMATCH_KEY}`
+        `https://api.codetabs.com/v1/proxy/?quest=api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MUSIXMATCH_KEY}`
       );
       const tracks = await res.json();
       setIsLoading(false);
---
+++ src/components/track_lyrics/Track_api.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { lyric } from "./modules";
+
+export const Track_api = (id: unknown) => {
+  const [lyric, setLyric] = useState<lyric | any>();
+  const [songDetails, setSongDetails] = useState<any>();
+  const [loading, setIsLoading] = useState(true);
+  const [error, setIsError] = useState<unknown>();
+
+  const get_api = async (song: unknown) => {
+    try {
+      const res = await fetch(
+        `https://api.codetabs.com/v1/proxy/?quest=api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${song}&apikey=${process.env.REACT_APP_MUSIXMATCH_KEY}`
+      );
+      const res_two = await fetch(
+        `https://api.codetabs.com/v1/proxy/?quest=api.musixmatch.com/ws/1.1/track.get?track_id=${song}&apikey=${process.env.REACT_APP_MUSIXMATCH_KEY}`
+      );
+      const data = await res.json();
+      const data_track = await res_two.json();
+      setIsLoading(false);
+      setSongDetails(data_track);
+      setIsLoading(false);
+      setLyric(data);
+    } catch (error) {
+      setIsError(error);
+    }
+  };
+
+  useEffect(() => {
+    get_api(id);
+  }, [id]);
+
+  return {
+    lyric,
+    songDetails,
+    loading,
+    error,
+  };
+};
