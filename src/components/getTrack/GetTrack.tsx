219e2edab11a724987cc00930b7e70d9dbffcde8
Riccardo Consolandi <rc@capsula.app>
2022-10-16 15:29:05 +0200

    feat: getTrack, songs; add new file to get tracks from songs
        - additionally add a css file for the getTrack file

---
+++ src/components/getTrack/GetTrack.tsx
@@ -0,0 +1,55 @@
+import {
+  faMusic,
+  faPlay,
+  faCompactDisc,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { Lyrics } from "../songs/lyrics/Lyrics";
+import "./getTrack.css";
+
+const GetTrack = ({ song }: any) => {
+  const limitText = (artist_name: string): string => {
+    if (artist_name.length > 30) return artist_name.substring(0, 30) + "...";
+    return artist_name;
+  };
+
+
+  return (
+    <div className="container_item">
+      <details>
+        <summary className="title">{limitText(song.track.artist_name)}</summary>
+        <div className="box_container">
+          <p>
+            <FontAwesomeIcon icon={faPlay} />
+            <strong> Track:</strong> {song.track.track_name}
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faCompactDisc} />
+            <strong> Album:</strong> {song.track.album_name}
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faMusic} />
+            <strong> Genre:</strong>
+            {
+              song.track.primary_genres.music_genre_list[0].music_genre
+                .music_genre_name
+            }
+          </p>
+          <Link
+            to={`/track/${song.track.track_id}`}
+            style={{ textDecoration: "none" }}
+          >
+            <Lyrics
+              track={song.track.track_id}
+              artist={song.track.artist_name}
+            />
+          </Link>
+        </div>
+      </details>
+    </div>
+  );
+};
+
+export default GetTrack;
--- src/components/songs/Songs.tsx
+++ src/components/songs/Songs.tsx
@@ -1,44 +1,21 @@
-import { Link, Route, Routes } from "react-router-dom";
 import { GetApi } from "../api/GetApi";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCompactDisc,
-  faMusic,
-  faPlay,
-} from "@fortawesome/free-solid-svg-icons";
 import { Lyrics } from "./lyrics/Lyrics";
-import "./songs.css";
 import { TrackLyric } from "../trackLyrics/TrackLyric";
 import { Loader } from "../loader/Loader";
 import { Search } from "../search/Search";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { SearchApi } from "../search/SearchApi";
 import { TrackApi } from "../trackLyrics/TrackApi";
+import GetTrack from "../getTrack/GetTrack";
+import { useEffect } from "react";
 
 export const Songs = () => {
-  const { lyrics, isLoading, isError } = GetApi();
+  const trackName = useSelector((state: any) => state.track.track);
+  const { lyrics, songDetails, isLoading, isError } = GetApi(trackName);
 
-
-
-  // const song_array: Array<string> = [
-  //   "David Guetta feat. Bebe Rexha",
-  //   "Coolio feat. L.V.",
-  //   "Lewis Capaldi",
-  //   "Sam Smith feat. Kim Petras",
-  //   "Ed Sheeran",
-  //   "Eliza Rose feat. Interplanetary Criminal",
-  //   "Lizzo",
-  //   "Cian Ducrot",
-  //   "OneRepublic",
-  //   "Harry Styles",
-  // ];
-
-  const limitText = (artist_name: string): string => {
-    if (artist_name.length > 30) return artist_name.substring(0, 30) + "...";
-    return artist_name;
-  };
-
+   console.log(trackName)
   if (isError) {
+    console.log(isError)
     return (
       <div>
         <h1 style={{ textAlign: "center" }}>
@@ -59,74 +36,11 @@ export const Songs = () => {
       <div className="container">
         {!isLoading &&
           lyrics.track_list !== undefined &&
-          lyrics.track_list.map((song: any) => {
-            return (
-              <div className="container_item" key={song.track.commontrack_id}>
-                <details>
-                  <summary className="title">
-                    {limitText(song.track.artist_name)}
-                  </summary>
-                  <div className="box_container">
-                    <p>
-                      <FontAwesomeIcon icon={faPlay} />
-                      <strong> Track:</strong> {song.track.track_name}
-                    </p>
-                    <p>
-                      <FontAwesomeIcon icon={faCompactDisc} />
-                      <strong> Album:</strong> {song.track.album_name}
-                    </p>
-                    <p>
-                      <FontAwesomeIcon icon={faMusic} />
-                      <strong> Genre:</strong>
-                      {song.track.primary_genres.music_genre_list[0] !==
-                        undefined &&
-                        song.track.primary_genres.music_genre_list[0]
-                          .music_genre.music_genre_name}
-                    </p>
-                    <Link
-                      to={`/track/${song.track.track_id}`}
-                      style={{ textDecoration: "none" }}
-                    >
-                      <Lyrics
-                        track={song.track.track_id}
-                        artist={song.track.artist_name}
-                      />
-                    </Link>
-                  </div>
-                </details>
-              </div>
-            );
-          })}
-        {/* {song_array.map((song: any) => {
-          return (
-            <div className="container_item">
-              <details>
-                <summary className="title">{song}</summary>
-                <div className="box_container">
-                  <p>
-                    <FontAwesomeIcon icon={faPlay} />
-                    <strong> Track:</strong> hello
-                  </p>
-                  <p>
-                    <FontAwesomeIcon icon={faCompactDisc} />
-                    <strong> Album:</strong> Come on
-                  </p>
-                  <p>
-                    <FontAwesomeIcon icon={faMusic} />
-                    <strong> Genre:</strong>
-                    Pop
-                  </p>
-                  <Link to={`/track/${song}`}>
-                    <Lyrics track={song} />
-                  </Link>
-                  <Routes>
-                    <Route path={`/track/${song}`} element={<TrackLyric />} />
-                  </Routes>
-                </div>
-              </details>
-            </div>
-          );
-        })} */}
+          (songDetails.length > 0 ? songDetails : lyrics.track_list).map(
+            (song: any) => {
+              return <GetTrack song={song} key={song.track.commontrack_id} />;
+            }
+          )}
       </div>
     </>
   );
