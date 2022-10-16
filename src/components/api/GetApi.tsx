bb2db1ae4c4fbd38d1dcfccc46bf86a8d3a7fcaa
Riccardo Consolandi <rc@capsula.app>
2022-10-16 15:26:10 +0200

    fix: getApi; place try catch inside the useEffect

--- src/components/api/GetApi.tsx
+++ src/components/api/GetApi.tsx
@@ -1,29 +1,36 @@
 import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
-export const GetApi = () => {
+export const GetApi = (song?: unknown) => {
   const [lyrics, setLyrics] = useState<any>();
+  const [songDetails, setSongDetails] = useState<any>();
   const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState<unknown>();
-
-  const getLyrics = async () => {
-    try {
-      const res = await fetch(
-        `https://api.codetabs.com/v1/proxy/?quest=api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MUSIXMATCH_KEY}`
-      );
-      const tracks = await res.json();
-      setIsLoading(false);
-      setLyrics({
-        track_list: tracks.message.body.track_list,
-        heading: "Top 10 Tracks",
-      });
-    } catch (error) {
-      setIsError(error);
-    }
-  };
+  const [isError, setIsError] = useState<any>();
+  const trackName = useSelector((state: any) => state.track.track);
 
   useEffect(() => {
+    const getLyrics = async (): Promise<void> => {
+      try {
+        const res = await fetch(
+          `https://api.codetabs.com/v1/proxy/?quest=api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MUSIXMATCH_KEY}`
+        );
+        const secondRes = await fetch(
+          `https://api.codetabs.com/v1/proxy/?quest=api.musixmatch.com/ws/1.1/track.search?q_track=${trackName}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MUSIXMATCH_KEY}`
+        );
+        const tracks = await res.json();
+        const dataTrack = await secondRes.json();
+        setIsLoading(false);
+        setLyrics({
+          track_list: tracks.message.body.track_list,
+          heading: "Top 10 Tracks",
+        });
+        setSongDetails(dataTrack.message.body.track_list);
+      } catch (error) {
+        setIsError(error);
+      }
+    };
     getLyrics();
-  }, []);
+  }, [trackName]);
 
-  return { lyrics, isLoading, isError };
+  return { lyrics, songDetails, isLoading, isError };
 };
